4c039ba53f26c3ffa23435c37e0186573a43ce5d
박찬규 <126973231+pck-llight@users.noreply.github.com>
2026-01-20 10:52:41 +0900

    Add custom font and SVG icon for plane
    - Introduced a new font "Geist Sans" with regular weight.
    - Added an SVG representation of a plane for use in the application.

--- app/components/step_bar/StepBar.css.ts
+++ app/components/step_bar/StepBar.css.ts
@@ -1,11 +1,19 @@
 import {style} from "@vanilla-extract/css";
 
-export const stepBarContainer = style({
+export const stepBarBox = style({
     display: "flex",
-    position: "absolute",
+    width: "100dvw",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "27px",
+});
+
+export const stepBarContainer = style({
+    width: "fit-content",
+    display: "flex",
     padding: "4px",
     flexDirection: "column",
-    alignItems: "flex-start",
+    alignItems: "center",
     gap: "4px",
     borderRadius: "12px",
     border: "1px solid rgba(255, 255, 255, 0.68)",
@@ -15,10 +23,42 @@ export const stepBarContainer = style({
 
 export const stepBarProgressBarContainer = style({
     display: "flex",
+    width: "100%",
     padding: "3px 5px",
     flexDirection: "column",
     alignItems: "flex-start",
     gap: "4px",
 });
 
-export const stepBarProgressBar = style({});
+export const stepBarProgressBar = style({
+    position: "relative",
+    width: "25%", //이거 나중에 값따라서 바뀌어야 함
+    height: "4px",
+    borderRadius: "4px",
+    background: "linear-gradient(90deg, rgba(16, 152, 255, 0.60) 0%, #1098FF 100%)",
+    '::after': {
+        content: "",
+        position: "absolute",
+        top: "-5.5px",
+        right: "-7px",
+        width:"16px",
+        height: "16px",
+        backgroundImage: "url(../../assets/plane.svg)",
+        backgroundSize: "contain",
+    }
+});
+
+export const stepBarButtonContainer = style({
+    display: "flex",
+    alignItems: "center",
+    gap: "4px",
+})
+
+export const stepBarButton = style ({
+    display: "flex",
+    padding: "10px 16px",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "10px",
+    color: "rgba(0, 0, 0, 0.38)",
+})
--- app/components/step_bar/StepBar.tsx
+++ app/components/step_bar/StepBar.tsx
@@ -1,7 +1,17 @@
-import { stepBarContainer } from "./StepBar.css";
+import { stepBarBox, stepBarContainer, stepBarButtonContainer, stepBarButton, stepBarProgressBarContainer, stepBarProgressBar } from "./StepBar.css";
 
 export default function StepBar() {
-    return <>
-        <div className={stepBarContainer}>sex</div>
-    </>;
+    return <div className={stepBarBox}>
+        <div className={stepBarContainer}>
+            <div className={stepBarButtonContainer}>
+                <div className={stepBarButton}>Trip</div>
+                <div className={stepBarButton}>Documents</div>
+                <div className={stepBarButton}>asdf</div>
+                <div className={stepBarButton}>asdf</div>
+            </div>
+            <div className={stepBarProgressBarContainer}>
+                <div className={stepBarProgressBar}/>
+            </div>
+        </div>
+    </div>;
 }
